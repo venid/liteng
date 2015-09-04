5148321f31d8361e22596458793cb3dc7c7f8605
venid <reversline@gmail.com>
2015-09-04 10:14:49 +0300

    Добавил модуль Physics

---
+++ mod/physics.cpp
@@ -0,0 +1,79 @@
+#include "physics.h"
+#include "object.h"
+#include "defs.h"
+#include "manager.h"
+#include "log.h"
+#include "component.h"
+#include "luastate.h"
+#include "typevar.h"
+
+META_METHODS(Physics,
+ METHOD(create, Physics::Create))
+META_PROPERTY(Physics)
+META_OBJECT(Physics, Physics, &Module::Instance)
+
+unsigned int Physics :: messages_list[] = {MSG_FINISH, MSG_ADD_UNIT, 0};
+
+/// -----------------------------------------------------------------------------------
+
+Physics :: Physics(const char* Name) : Module(Name)
+ { do_update = (MUpdate)&Physics::init_update;
+   tabMessages = messages_list;
+   metaClass = &Instance;
+ }
+
+void Physics :: set_var()
+ { pool.emplace(vDELTA_TIME, Link{1, new double});
+ }
+
+bool Physics :: init(Lua::State &lua)
+ {
+   LOG_INFO("%s: first init.", getName());
+   return true;
+ }
+
+bool Physics :: msg_processing()
+ { int tMsg, param;
+   Object* pobj;
+
+   while(Manager::getMessage(getNumQueue(), tMsg, pobj, param))
+    { switch(tMsg)
+       { case MSG_ADD_UNIT:
+          addComp((Unit*)pobj);
+          pobj->release();
+          break;
+         case MSG_FINISH:
+          do_update = (MUpdate)&Physics::clear_update;
+          return false;
+       }
+    }
+   return true;
+ }
+
+int Physics :: init_update(double tm)
+ { Manager :: sendMessage(MSG_ACTIVE, nullptr, getNumQueue());
+
+   set_var();
+   LOG_INFO("%s: second init.", getName());
+   do_update = (MUpdate)&Physics::run_update;
+   return 1;
+ }
+
+int Physics :: clear_update(double tm)
+ { delComp();
+   pool[vDELTA_TIME].refCount --;
+   del_var();
+   LOG_INFO("%s: clear.", getName());
+   do_update = &Module::empty_update;
+   return 0;
+ }
+
+int Physics :: run_update(double tm)
+ { *mp_delta = tm;
+
+   if(msg_processing())
+    { for(auto &it : components)
+       it.second->update();
+    }
+   return 1;
+ }
---
+++ mod/physics.h
@@ -0,0 +1,32 @@
+
+#pragma once
+
+#include "module.h"
+
+namespace Lua { class State; }
+
+class Physics : public Module
+ { private:
+    double* mp_delta;
+
+    bool init(Lua::State &lua);
+    bool msg_processing();
+    void set_var();
+
+    int init_update(double);
+    int run_update(double);
+    int clear_update(double);
+
+   public:
+    Physics(const char* Name);
+    ~Physics() {}
+
+    static Object* Create(const char* Name, Lua::State* lua)
+     { Physics *phys = new Physics(Name);
+       phys->init(*lua);
+       return phys;
+     }
+
+    static Meta::Base Instance;
+    static unsigned int messages_list[];
+ };
