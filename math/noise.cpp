3ab875577f5ec53ee81ea341c2f1bb806d582c3b
venid <reversline@gmail.com>
2015-09-11 14:33:41 +0300

    Добавил шум Перлина и генератор случайных чисел (github.com/jj1bdx/TinyMT/)

---
+++ math/noise.cpp
@@ -0,0 +1,137 @@
+
+#include "noise.h"
+#include "tinymt32.h"
+
+
+// -------------------------------------------------------------------------------------
+#define B SAMPLE_SIZE
+#define BM (SAMPLE_SIZE-1)
+
+#define N 0x1000
+#define NP 12   /* 2^N */
+#define NM 0xfff
+
+#define s_curve(t) ( t * t * (3.0f - 2.0f * t) )
+#define lerp(t, a, b) ( a + t * (b - a) )
+
+#define setup(i,b0,b1,r0,r1)\
+	t = vec[i] + N;\
+	b0 = ((int)t) & BM;\
+	b1 = (b0+1) & BM;\
+	r0 = t - (int)t;\
+	r1 = r0 - 1.0f;
+
+Noise::Perlin :: Perlin(int seed)
+ { m_octaves = 1;
+   m_frequency = 1.f;
+   m_amplitude = 1.f;
+   init(seed);      
+ }
+
+void Noise::Perlin :: init(int seed)
+ { int i, j, k;
+   tinymt32_t tinymt;
+   glm::vec2 v2;
+   glm::vec3 v3;
+
+   tinymt.mat1 = 0x8f7011ee;
+   tinymt.mat2 = 0xfc78ff1f;
+   tinymt.tmat = 0x3793fdff;
+   tinymt32_init(&tinymt, seed);
+	  for (i = 0 ; i < SAMPLE_SIZE ; i++)
+    { p[i] = i;
+		    g1[i] = (float)((tinymt32_generate_uint32(&tinymt) % (B + B)) - B) / B;
+		    v2.x = (float)((tinymt32_generate_uint32(&tinymt) % (B + B)) - B) / B;
+      v2.y = (float)((tinymt32_generate_uint32(&tinymt) % (B + B)) - B) / B;
+		    v2 = glm::normalize(v2);
+      g2[i][0] = v2.x;
+      g2[i][1] = v2.y;
+		    v3.x = (float)((tinymt32_generate_uint32(&tinymt) % (B + B)) - B) / B;
+      v3.y = (float)((tinymt32_generate_uint32(&tinymt) % (B + B)) - B) / B;
+      v3.z = (float)((tinymt32_generate_uint32(&tinymt) % (B + B)) - B) / B;
+		    v3 = glm::normalize(v3);
+      g3[i][0] = v3.x;
+      g3[i][1] = v3.y;
+      g3[i][2] = v3.z;
+	   }
+
+	   while (--i)
+     { k = p[i];
+		     p[i] = p[j = tinymt32_generate_uint32(&tinymt) % B];
+		     p[j] = k;
+	    }
+
+	   for (i = 0 ; i < B + 2 ; i++)
+     { p[B + i] = p[i];
+		     g1[B + i] = g1[i];
+		     for (j = 0 ; j < 2 ; j++)
+			     g2[B + i][j] = g2[i][j];
+		     for (j = 0 ; j < 3 ; j++)
+			     g3[B + i][j] = g3[i][j];
+	    }
+         
+ }
+
+float Noise::Perlin :: noise(float &vec)
+ { float result = 0.f;
+   
+
+   return result;
+ }
+
+float Noise::Perlin :: noise(glm::vec2 &vec)
+ { float result = 0.f;
+   
+
+   return result;
+ }
+
+float Noise::Perlin :: noise(glm::vec3 &vec)
+ { int bx0, bx1, by0, by1, bz0, bz1, b00, b10, b01, b11;
+	  float rx0, rx1, ry0, ry1, rz0, rz1, *q, sy, sz, a, b, c, d, t, u, v;
+	  int i, j;
+
+	  setup(0, bx0,bx1, rx0,rx1);
+	  setup(1, by0,by1, ry0,ry1);
+	  setup(2, bz0,bz1, rz0,rz1);
+
+	  i = p[ bx0 ];
+	  j = p[ bx1 ];
+
+	  b00 = p[ i + by0 ];
+	  b10 = p[ j + by0 ];
+	  b01 = p[ i + by1 ];
+	  b11 = p[ j + by1 ];
+
+	  t  = s_curve(rx0);
+	  sy = s_curve(ry0);
+	  sz = s_curve(rz0);
+
+   #define at3(rx,ry,rz) ( rx * q[0] + ry * q[1] + rz * q[2] )
+
+	  q = g3[ b00 + bz0 ] ; u = at3(rx0,ry0,rz0);
+	  q = g3[ b10 + bz0 ] ; v = at3(rx1,ry0,rz0);
+	  a = lerp(t, u, v);
+
+	  q = g3[ b01 + bz0 ] ; u = at3(rx0,ry1,rz0);
+	  q = g3[ b11 + bz0 ] ; v = at3(rx1,ry1,rz0);
+	  b = lerp(t, u, v);
+
+	  c = lerp(sy, a, b);
+
+	  q = g3[ b00 + bz1 ] ; u = at3(rx0,ry0,rz1);
+	  q = g3[ b10 + bz1 ] ; v = at3(rx1,ry0,rz1);
+	  a = lerp(t, u, v);
+
+	  q = g3[ b01 + bz1 ] ; u = at3(rx0,ry1,rz1);
+	  q = g3[ b11 + bz1 ] ; v = at3(rx1,ry1,rz1);
+	  b = lerp(t, u, v);
+
+	  d = lerp(sy, a, b);
+
+	  return lerp(sz, c, d);
+ }
+
+// -------------------------------------------------------------------------------------
+
+
---
+++ math/noise.h
@@ -0,0 +1,98 @@
+
+#pragma once
+
+#include <math.h>
+#include <glm/glm.hpp>
+
+namespace Noise
+{
+
+inline float bias( float x, float a )
+ { return x / ((1/a - 2)*(1 - x) + 1); }
+
+inline float gain( float x, float a )
+ { if( x < 0.5 )
+    return x / ((1/a - 2)*(1 - 2*x) + 1);
+   else
+    return ((1/a - 2)*(1 - 2*x) - x) / ((1/a - 2)*(1 - 2*x) - 1);
+ }
+
+inline float gaussian( float x, float stdDev )
+ { return exp( -x*x / (2*stdDev*stdDev) ); }
+
+
+
+#define SAMPLE_SIZE 512
+        
+class Perlin
+ { private:
+    int   m_octaves;
+    float m_frequency;
+    float m_amplitude;
+
+    int p[SAMPLE_SIZE + SAMPLE_SIZE + 2];
+    float g3[SAMPLE_SIZE + SAMPLE_SIZE + 2][3];
+    float g2[SAMPLE_SIZE + SAMPLE_SIZE + 2][2];
+    float g1[SAMPLE_SIZE + SAMPLE_SIZE + 2];
+
+    void init(int seed);
+
+    float noise(float &);
+    float noise(glm::vec2 &);
+    float noise(glm::vec3 &);
+
+    template <typename Type>
+    float frac_noise(Type &arg)
+     { float result = 0.f;
+       float amp = m_amplitude;
+       Type vec = arg * m_frequency; 
+       
+	      for(int i = 0; i < m_octaves; i++)
+	       { result += noise(vec) * amp;
+		        vec *= 2.0f;
+		        amp *= 0.5f;
+        }
+	      return result;
+     }
+
+   public:
+    Perlin(int seed);
+
+    void setOctaves(int n)
+     { m_octaves = n; }
+    void setWave(float freq, float amp)
+     { m_frequency = freq;
+       m_amplitude = amp;
+     }
+
+    inline float get(float n)
+     { return frac_noise(n); }
+    inline float get(glm::vec2& v)
+     { return frac_noise(v); }
+    inline float get(glm::vec3& v)
+     { return frac_noise(v); }
+
+    inline float getClean(float n)
+     { return noise(n); }
+    inline float getClean(glm::vec2& v)
+     { return noise(v); }
+    inline float getClean(glm::vec3& v)
+     { return noise(v); } 
+ };
+
+// ----------------------------------------------- 
+
+float cell( float x );
+float cell( glm::vec2& x );
+float cell( glm::vec3& x );
+
+void  cellN( const glm::vec2& x, int n, float* values );
+void  cellN( const glm::vec3& x, int n, float* values );
+
+glm::vec2 cell2( float x );
+glm::vec2 cell2( glm::vec2& x );
+
+glm::vec3 cell3( float x );
+glm::vec3 cell3( glm::vec3& x );
+
+}
